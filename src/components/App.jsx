4aa19d22296cc5ca1b6238d01f4bc9ce05b72dc2
Chandler Ocapan <Cocapan@gmail.com>
2021-09-29 14:33:56 -0700

    Implemented CSS transitions to the ProductCard component with the use of React Transition Groups

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-quotes */
 import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import AppHeader from './AppHeader.jsx';
 import Introduction from './Introduction.jsx';
 import SkillSet from './SkillSet.jsx';
@@ -10,19 +11,27 @@ import '../ChandlerWebsite.scss';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { appear: true };
   }
 
   render() {
+    const { appear } = this.state;
     return (
       <div id='AppContainer'>
         <AppHeader />
-        <div id='AppBody'>
-          <Introduction />
-          <SkillSet />
-          <AboutMe />
-          <Projects />
-        </div>
+        <CSSTransition
+          in={appear}
+          appear={true}
+          timeout={300}
+          classNames='fade'
+        >
+          <div id='AppBody'>
+            <Introduction />
+            <SkillSet />
+            <AboutMe />
+            <Projects />
+          </div>
+        </CSSTransition>
       </div>
     );
   }
